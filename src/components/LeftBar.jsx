06b80326e1b15759bbddf8bbd3f370e33ae33412
Aryanverma09 <iamaryanverma5555@gmail.com>
2026-01-26 23:22:46 +0530

    change

--- src/components/LeftBar.jsx
+++ src/components/LeftBar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const LeftBar = () => {
   return (
-    <div className='flex justify-center items-center w-[20vw] border-l-2'>LeftBar</div>
+    <div className='flex justify-center bg-gray-400 items-center w-[20vw] border-l-2'>LeftBar</div>
   )
 }
 
