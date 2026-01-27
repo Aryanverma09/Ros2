c5d725d0a7b18115b8d1b2e78dfad9fcadb29462
Aryanverma09 <iamaryanverma5555@gmail.com>
2026-01-27 19:59:10 +0530

    new

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -7,7 +7,7 @@ const Display = () => {
         <img
         alt="Camera feed"
           src="http://localhost:8080/stream?topic=camera/image_raw&transport=compressed"
-          className="h-full w-full object-cover"
+          className="h-full w-full object-cover "
         />
       </div>
       <div className="h-[50vh] w-[35vw] border-2 rounded-2xl flex justify-center items-center overflow-hidden">
