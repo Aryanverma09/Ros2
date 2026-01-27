875a73d171b773febac82be588cda2315fa0a0b6
Aryanverma09 <iamaryanverma5555@gmail.com>
2026-01-27 19:52:29 +0530

    Done

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -8,8 +8,8 @@ const Dashboard = () => {
     <div className="flex">
       <div className=" flex  justify-center items-center h-screen w-[80vw] flex-col gap-10">
         <div className="nav flex h-[6vh] w-[50vw] rounded-2xl bg-gray-500 justify-around items-center ">
-          <h1>Ros Connection</h1>
-          <h1>Ros Connection</h1>
+          <h1>✅Ros Connection</h1>
+          <h1>✅Camera Connection</h1>
         </div>
         <Display />
         <Control />
